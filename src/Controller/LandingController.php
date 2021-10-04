f7268a4ff9a3c70dfbf8a41ab4c6c917200c2cf1
atom-box <atom_box@hotmail.com>
2021-10-04 13:08:48 -0500

    First sample page: landing prints a simple plaintext hello.

---
+++ src/Controller/LandingController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/Landing.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingController extends AbstractController
+{
+    /**
+     * @Route("/", name="landing")
+     */
+    public function list(): Response
+    {
+        // list() DOES NOT NEED TO CALL: THE ROUTE SETS IT IN MOTION
+        // return $this->render('product/index.html.twig', []); 
+        return new Response('<h1>Atlas Improv</h1>');
+    }
+}
