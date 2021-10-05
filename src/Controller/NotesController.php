b7f7576c57b5eb5d5e15e3be2d2d0b38823f7950
atom-box <atom_box@hotmail.com>
2021-10-05 14:44:17 -0500

    Link up routes so notes and alarms both reachable. Give ALARMS some content.

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -4,6 +4,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Forms;
+// 
+use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Security\Csrf\TokenGenerator\UriSafeTokenGenerator;
+use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
+
+
+
 
 class NotesController extends AbstractController
 {
@@ -12,6 +22,12 @@ class NotesController extends AbstractController
      */
     public function showNotes(): Response
     {
+        $formFactory = Forms::createFormFactory();
+
+        //  Todo. Protection against CSRF attacks is built into the Form component, but you need to explicitly enable it. See more at
+        // at the docs     https://symfony.com/doc/current/components/form.html#csrf-protection
+
+
         return $this->render('notes/index.html.twig', [ ]);
 
     }
